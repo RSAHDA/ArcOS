01955e6fbc9a58e0dc37e643e5dc0ae8ec1dc73e
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-01-03 21:07:22 -0500

    doing progress

--- computation-module/computation-module.ino
+++ computation-module/computation-module.ino
@@ -1,10 +1,22 @@
-#include ""
+#include "libraries/memory-bus/memory-bus.h"
+
+Master file_loader(8);
 
 void setup() {
+  // -------------------------------------------------------------
   // load files from flash and store in SRAM
+  // -------------------------------------------------------------
+
+  // run Master instance:
+  file_loader.run();
+
+  // send initial request:
+  file_loader.send_to_slave(0);
+
+  // request slave:
+  String dirrectory-array = file_loader.request_slave();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
