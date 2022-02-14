14ed1184a14ed327b31a32898c4731cc853b0ceb
rishitv26 <75286874+rishitv26@users.noreply.github.com>
2022-02-13 22:23:21 -0500

    doing progress

--- computation-module/computation-module.ino
+++ computation-module/computation-module.ino
@@ -1,11 +1,16 @@
-# include "modules/load_file.h"
+# include "modules/file_system.h"
 
 FileSystem::textFile file("rishit", "Rishit is a great boy", "L:/");
 
 void setup() {
-  Serial.begin(9600); 
+  // to always initialize for the first time:
+  EEPROM.write(0, 0);
+  // --------- rest of booting up: -----------
+  Serial.begin(9600);
+  FileSystem::getFiles();
+  Serial.println("DONE!!!!");
 }
 
 void loop() {
-  Serial.println(file.returnFile());  
+
 }
--- computation-module/modules/file_system.h
+++ computation-module/modules/file_system.h
@@ -1,5 +1,5 @@
 //
-// Created by Yaten on 13-02-2022.
+// Created by Rishit on 13-02-2022.
 //
 
 #ifndef ARCOS_LOAD_FILE_H
@@ -7,6 +7,7 @@
 
 #include "string.h"
 #include "Arduino.h"
+#include <EEPROM.h>
 
 // There will be 2 primary files in the beginning:
 // -- .txt
@@ -16,30 +17,35 @@
 // -- .app (execute\able file for any application)
 // -- .loc (locates or references a file)
 
-// META DATA FORMAT: .(file type) * (file name) + (file location) | (file data)
+// META DATA FORMAT: .(file type)*(file name)+(file location)|(file data)
 // not a lot in metadata since processor doesn't have good memory.
 
 
 // ------------ User File -----------
 namespace FileSystem {
-    const unsigned int ITEM_SPACE = 276;
-    char16_t SystemSpace[ITEM_SPACE];
-    const int MAX_MEMORY_AVAILABLE = 0; // .app max length * ITEM_SPACE
+    const unsigned int ITEM_SPACE = 100;
+    char *memoryPointer[ITEM_SPACE];
+
+    char *getFiles() {
+        for (int i = 0; i < 1024; ++i) {
+            // todo
+        }
+    }
 
     class textFile {
     private:
-        char filename[90];
-        char data[90];
-        char folder[90];
+        char filename[30];
+        char data[30];
+        char folder[30];
     public:
-        textFile(char name[90], char d[90], char loc[90]) {
-            strncpy(filename, name, 90);
-            strncpy(data, d, 90);
-            strncpy(folder, loc, 90);
+        textFile(char name[30], char d[30], char loc[30]) {
+            strncpy(filename, name, 30);
+            strncpy(data, d, 30);
+            strncpy(folder, loc, 30);
         };
 
         String returnFile() {
-            return ".txt * " + String(this->filename) + " + " + String(this->folder) + " | " + String(this->data);
+            return ".txt*" + String(this->filename) + "+" + String(this->folder) + "|" + String(this->data);
         };
 
         char *returnData() {
